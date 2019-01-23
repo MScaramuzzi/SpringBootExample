c9df5dec8cf3b9293fc45d06c02c325a53588732
Andrea Fornaia <fornaia.andrea@gmail.com>
2019-01-23 19:28:48 +0100

    After unzip alien.zip (Spring Initializr maven skeleton)

---
+++ alien/src/main/java/com/space/scanner/alien/AlienApplication.java
@@ -0,0 +1,14 @@
+package com.space.scanner.alien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlienApplication.class, args);
+	}
+
+}
+
