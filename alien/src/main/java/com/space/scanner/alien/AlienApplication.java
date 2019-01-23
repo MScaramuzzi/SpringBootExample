c872765024d438d06f3cf74c175413ea4f11b83c
Andrea Fornaia <fornaia.andrea@gmail.com>
2019-01-24 00:44:19 +0100

    Hello Planet

--- alien/src/main/java/com/space/scanner/alien/AlienApplication.java
+++ alien/src/main/java/com/space/scanner/alien/AlienApplication.java
@@ -2,12 +2,32 @@ package com.space.scanner.alien;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.space.scanner.alien.bean.Mars;
+import com.space.scanner.alien.bean.Planet;
+import com.space.scanner.alien.bean.Venus;
+
 
 @SpringBootApplication
 public class AlienApplication {
 
+	private String planet = "venus";
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AlienApplication.class, args);
+	}
+	
+	@Bean
+	public Planet planet() {
+		System.out.println("Landing planet: " + planet);
+		
+		switch (planet) {	
+			case "mars": 	return new Mars(); 
+			case "venus": 	return new Venus(); 
+			
+			default:		return new Mars(); 
+		}
 	}
 
 }
